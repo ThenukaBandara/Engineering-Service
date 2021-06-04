0c2e7389662e8ad600faf5c7b0a39a61c12d5b60
ThenukaBandara <tharinduepay@gmail.com>
2021-06-04 18:03:22 +0530

    Engineering Service
    New test project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let webName='Engineering Service';
+
+console.log(webName);
+
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+	document.querySelector(".container").classList.toggle ("change");
+});
+
