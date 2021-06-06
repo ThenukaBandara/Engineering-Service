ae6de89f94ad65fe548155a147f5e72e86bba157
ThenukaBandara <tharinduepay@gmail.com>
2021-06-06 15:33:31 +0530

    Loading spinner

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-let webName='Engineering Service';
-
-console.log(webName);
-
+window.onload = () => {
+	setTimeout(() => {
+		document.querySelector("body").classList.add ("display");
+	},2000);
+};
 
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
 	document.querySelector(".container").classList.toggle ("change");
